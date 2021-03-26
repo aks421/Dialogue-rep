17b0be424fbd2c0ede06c5331516bace70295a39
aks421 <61337846+aks421@users.noreply.github.com>
2021-03-25 19:03:34 -0500

    Initial project
    Contains the dialogue system in its current state. The text successfully shows at the top of the screen but it is not the text from the yarn file. The buttons also don't show, but they are in the project.

---
+++ Assets/PointingRay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointingRay : MonoBehaviour
+{
+    [SerializeField]
+    Camera cam;
+    Vector3 pos = new Vector3(200, 200, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = cam.ScreenPointToRay(pos);
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+    }
+}
